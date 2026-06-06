9d2c08480097c60c45a1c45580df787109f4ca20
Nguyễn Hải Ban <tonydevhehe@gmail.com>
2026-06-06 13:04:17 +0700

    [FEAT] Thêm kiểm tra lỗi input

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,28 +1,50 @@
-function TaskForm({isOpen,onClose})
-{
-    return(
+import { useState } from "react"
+function TaskForm({ isOpen, onClose }) {
+
+    const [taskName, setTaskName] = useState("")
+    const [error, setError] = useState({})
+    function handleAdd() {
+        const newError = {}
+        if (taskName.trim().length > 100)
+           {
+             newError.text = "Khong duoc nhap qua 100 ki tu"
+           }
+           if(taskName.trim()==="")
+                newError.text="Khong duoc bo trong"
+        setError(newError)
+        if (Object.keys(newError).length > 0)
+        {
+            return
+        }
+
+    }
+    return (
         <>
-        <div className={`modal-overlay ${isOpen ? "modal-start":""}`}>
-        <div className="modal-content d-flex flex-column gap-15">
-            <p className="text-title" >Add task</p>
-            <div className="d-flex flex-column gap-10">
-                <p className="label">Task</p>
-                <input type="text" className="input" placeholder="Type your task here..."/>
-            </div>
-            <div className="d-flex flex-column gap-10">
-                <p className="label">Priority</p>
-                <div className="d-flex gap-10">
-                    <button className="btn btn-high">High</button>
-                    <button className="btn btn-medium">Medium</button>
-                    <button className="btn btn-low">Low</button>
+            <div className={`modal-overlay ${isOpen ? "modal-start" : ""}`}>
+                <div className="modal-content d-flex flex-column gap-15">
+                    <p className="text-title" >Add task</p>
+                    <div className="d-flex flex-column gap-10">
+                        <p className="label">Task</p>
+                        <input type="text" className="input" placeholder="Type your task here..."
+                            value={taskName}
+                            onChange={(e) => setTaskName(e.target.value)}
+                        />
+                        {error.text && (<p className="text-error">{error.text}</p>)}
+                    </div>
+                    <div className="d-flex flex-column gap-10">
+                        <p className="label">Priority</p>
+                        <div className="d-flex gap-10">
+                            <button className="btn btn-high">High</button>
+                            <button className="btn btn-medium">Medium</button>
+                            <button className="btn btn-low">Low</button>
+                        </div>
+                        <div className="d-flex flex-end">
+                            <button className="btn bg-primary" onClick={handleAdd}>Add</button>
+                        </div>
+                        <button className="btn btn-close" onClick={onClose}>X</button>
+                    </div>
                 </div>
-                <div className="d-flex flex-end">
-                    <button className="btn bg-primary">Add</button>
-                </div>
-                <button className="btn btn-close" onClick={onClose}>X</button>
             </div>
-        </div>
-    </div>
         </>
     )
 }
